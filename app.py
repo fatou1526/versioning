781dd49730455fd07de00b7d2b9015d2d6d7c0fa
Fatou <ndeyefatousene1502@gmail.com>
2023-08-11 15:10:36 +0000

    Printing the shape

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ import matplotlib.pyplot as plt
 # Loading data
 df = pd.read_csv("C:/Users/USER/Documents/Master2 DIT/Outil versioning/versioning/Crop_recommendation.csv")
 df.head()
+print(df.shape)
 
