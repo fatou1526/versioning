14ee35c1a7e2eaebf0316435ff375eac946fb049
Fatou <ndeyefatousene1502@gmail.com>
2023-08-14 09:55:30 +0000

    Still edit script

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-# Importing packages
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Loading data
-df = pd.read_csv("Crop_recommendation.csv")
-df.head()
-print(df.shape)
+print('hello world')
 
